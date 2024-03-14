37b28b665773ed87502bc9d6c09af82c8218e58f
code@stackfrontier.com <code@stackfrontier.com>
2024-03-15 00:41:32 +0530

    Initial Commit

---
+++ BusBuddyMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BusBuddyMAUI
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ BusBuddyMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BusBuddyMAUI
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
